052d5c8fa58dccab254c53611b3ab2f96a230679
Christopher Lewis <cjoellewis@yahoo.com>
2020-06-05 15:02:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
-end
+  attr_accessor :side_1, :side_2, :side_3
+    
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      begin
+        raise TriangleError
+      rescue
+        raise TriangleError => error
+          puts error.message
+        end
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      begin
+        raise TriangleError
+      rescue
+        raise TriangleError => error
+          puts error.message
+        end
+    elsif (@side_1 == @side_2) && (@side_2 == @side_3) 
+      return :equilateral
+    elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3) 
+      return :isosceles
+    else (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3) 
+      return :scalene
+      end
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "The sum of the lengths of two sides must exceed the length of the third side."
+    end
+  end
